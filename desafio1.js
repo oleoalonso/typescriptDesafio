3d3f1750bc3ce7206d374211361863b84fa7510e
Leonardo Alonso <oleoalonso@gmail.com>
2022-07-27 19:35:45 -0300

    desafio typescript

---
+++ desafio1.js
@@ -0,0 +1,11 @@
+// Como podemos rodar isso em um arquivo .ts sem causar erros? 
+
+// let employee = {};
+// employee.code = 10;
+// employee.name = "John";
+
+
+const funcionario = {
+    codigo: 10,
+    nome: 'John'
+};
---
+++ desafio2.js
@@ -0,0 +1,25 @@
+// Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {
+    nome: "maria",
+    idade: 29,
+    profissao: "atriz" 
+};
+
+let pessoa2 = {
+    nome: "roberto",
+    idade: 19,
+    profissao: "padeiro"
+}
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "atriz"
+};
+
+let pessoa4 = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
